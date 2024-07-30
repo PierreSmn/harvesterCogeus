65f6958042492a0630e562149edaf3c0813a930d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-07-30 13:48:41 +0000

    les plages

---
+++ lib/app/plages/plages_model.dart
@@ -0,0 +1,23 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'plages_widget.dart' show PlagesWidget;
+import 'package:flutter/material.dart';
+
+class PlagesModel extends FlutterFlowModel<PlagesWidget> {
+  ///  Local state fields for this page.
+
+  bool? checked;
+
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // State field(s) for Checkbox widget.
+  bool? checkboxValue;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+  }
+}
